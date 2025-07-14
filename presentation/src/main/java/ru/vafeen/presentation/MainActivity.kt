21d022669cac52880fa20300b28d6eda9bc21347
Vafeen <666av6@gmail.com>
2025-07-15 01:42:49 +0300

    9 custom theme (#14)

--- presentation/src/main/java/ru/vafeen/presentation/MainActivity.kt
+++ presentation/src/main/java/ru/vafeen/presentation/MainActivity.kt
@@ -10,20 +10,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
-import ru.vafeen.domain.network.repository.CharacterRemoteRepository
 import ru.vafeen.presentation.ui.screen.main_screen.CharactersScreen
-import ru.vafeen.presentation.ui.theme.RickAndMortyCharactersTheme
-import javax.inject.Inject
+import ru.vafeen.presentation.ui.theme.MainTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var characterRemoteRepository: CharacterRemoteRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RickAndMortyCharactersTheme {
+            MainTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(
                         modifier = Modifier.padding(innerPadding)
